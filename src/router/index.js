e687164993f6a008d55851c3d22ede97bdcc835b
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-03-14 10:47:06 +0000

    Removed hello world and added home page.

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 // @ts-ignore
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import HomePage from '../views/HomePage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: HomePage
   },
   {
     path: '/about',
--- src/tests/unit/Home.spec.js
+++ src/tests/unit/Home.spec.js
@@ -1,11 +1,11 @@
 // src/components/HelloWorld.spec.js
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../../../src/components/HelloWorld.vue'
+import Home from '../../components/Home.vue'
 
-describe('HelloWorld', () => {
+describe('Home Page', () => {
   it('should display header text', () => {
     const msg = 'Hello Vue 3'
-    const wrapper = mount(HelloWorld, { props: { msg } })
+    const wrapper = mount(Home, { props: { msg } })
 
     expect(wrapper.find('h1').text()).toEqual(msg)
   })
